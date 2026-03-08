5ee9e1fe9386a70ca158c5efee766c93377d3fa1
Kevin Rapa <krapa1@jh.edu>
2026-03-07 20:38:57 -0500

    Added main function

---
+++ perlinNoise/src/main/backend/main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+#include "field.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		printf("Require a numerical seed\n");
+		return EXIT_FAILURE;
+	}
+
+	long seed = atol(argv[1]);	
+
+	pixel_t *field = fieldAlloc();
+	int success = createField(field, seed, 0, 0);
+	printf("Return value: %d\n", success);
+
+	return success;
+}
